61e6695adf794340b512b76caf48c74f0d43607a
damienn7 <damien.nicolleau@epitech.eu>
2023-07-18 12:39:33 +0200

    ADD - ajout des conditions de prioritées à l'intérieur des parenthèses

--- index.php
+++ index.php
@@ -14,8 +14,13 @@ function eval_expr($expr)
 
     $sub_result = null;
 
-    $data = create_array_with_float($expr);
+    $data_array = @create_array_with_float($expr);
 
+    $data = $data_array[0];
+
+    $decimals = $data_array[1];
+
+    // var_dump($data);
     for ($i = 0; $i < count($data); $i++) {
 
         if ($data[$i] === "(") {
@@ -24,7 +29,7 @@ function eval_expr($expr)
 
         if ($data[$i] === ")" && $calc === true) {
             $calc = false;
-            $sub_result = calc_subexpr($subexpr);
+            $sub_result = @calc_subexpr($subexpr);
             $result .= $sub_result;
         }
 
@@ -36,23 +41,35 @@ function eval_expr($expr)
     }
 
     if (array_search(")",str_split($result)) !== false || array_search("(",str_split($result)) !== false) {
-        eval_expr($result);
+        @eval_expr($result);
     }
 
-    $sub_result = calc_subexpr(create_array_with_float($result));
+    // echo "result : ".$result."\nsub_result : ".$sub_result."\n";
+    $sub_result = @calc_subexpr(create_array_with_float($result)[0],true);
 
-    return $sub_result==""?$result."\n":$sub_result."\n";
+    if ($sub_result===true) {
+        @eval_expr($result);
+    }
+
+    return (double) $sub_result==""?$result."\n":$sub_result."\n";
 }
 
 function create_array_with_float($str){
     $array1 = str_split($str);
     $array_with_float = array();
     $number = null;
+    $j=0;
     for ($i=0; $i < count($array1); $i++) { 
         if (($array1[$i]!=="%"&&$array1[$i]!=="+")&&($array1[$i]!=="-"&&$array1[$i]!=="/")&&($array1[$i]!=="*"&&$array1[$i]!=="("&&$array1[$i]!==")")) {
             $number.=$array1[$i];
+            if ($array1[$i-1]===".") {
+                $j++;
+            }
             if ($i==count($array1)-1) {
-                array_push($array_with_float,$array1[$i]);
+                // echo $number;
+                // echo "number : ".$number."\narray i : ".$array1[$i]."\n";
+                $last_number = $number;
+                array_push($array_with_float,$last_number);
             }
         }else{
             array_push($array_with_float,$number);
@@ -61,16 +78,28 @@ function create_array_with_float($str){
         }
     }
 
-    return $array_with_float;
+    // var_dump($array_with_float);
+
+    return [$array_with_float,$j];
 }
 
-function calc_subexpr($subexpr)
+function calc_subexpr($subexpr,$check=false)
 {
+    
+    $last_calc = null;
     $sub_result = null;
-    for ($i = 0; $i < count($subexpr); $i++) {
-        switch ($subexpr[$i]) {
-            case '+':
-                $sub_result = $sub_result === null?$subexpr[$i - 1] + $subexpr[$i + 1]:$sub_result + $subexpr[$i+1];
+    if ($check===false) {
+        
+        for ($i = 0; $i < count($subexpr); $i++) {
+            switch ($subexpr[$i]) {
+                case '+':
+                    if ($subexpr[$i+2]==="/"||$subexpr[$i+2]==="%"||$subexpr[$i+2]==="*") {
+                        $last_calc .= '+'.$subexpr[$i-1];
+                        // echo "subexxxxpr".$subexpr[$i-1]."\n last_calc : ".$last_calc."\n";
+                        $sub_result= null;
+                }else{
+                    $sub_result = $sub_result === null?$subexpr[$i - 1] + $subexpr[$i + 1]:$sub_result + $subexpr[$i+1];
+                }
                 $i++;
                 break;
             case '-':
@@ -79,6 +108,9 @@ function calc_subexpr($subexpr)
                 break;
             case '/':
                 $sub_result = $sub_result === null?$subexpr[$i - 1] / $subexpr[$i + 1]:$sub_result / $subexpr[$i+1];
+                // echo "last : ".$subexpr[$i - 1]."\n";
+                // echo "sub_result : ".$sub_result."\n";
+                // echo "next : ".$subexpr[$i + 1]."\n";
                 $i++;
                 break;
             case '*':
@@ -90,10 +122,25 @@ function calc_subexpr($subexpr)
                 $i++;
                 break;
             default:
-                break;
+            break;
         }
         
     }
-
+    if ($last_calc!== null) {
+        $sub_result.= $last_calc;
+        // echo "sub_result : ".$sub_result."\n";
+        $sub_result=@calc_subexpr(create_array_with_float($sub_result)[0]);
+        // echo "sub_result : ".$sub_result."\n";
+    }
     return $sub_result;
+}else{
+    foreach ($subexpr as $key => $value) {
+        if (!is_numeric($value)&&$value!=".") {
+            // echo "value : ".$value;
+            return @calc_subexpr($subexpr);
+        }
+    }
+}
+
+
 }
