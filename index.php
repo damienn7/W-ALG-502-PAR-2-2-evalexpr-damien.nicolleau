caf27868b1dd485db2a337e4b92eb8851702ed36
damienn7 <damien.nicolleau@epitech.eu>
2023-07-23 11:40:48 +0200

    ADD - ajout de la version à jour de l'evalexpr qui fonctionne [last update]

--- index.php
+++ index.php
@@ -3,147 +3,218 @@
 // principale fonction de l'evalexpr alogorithme
 function eval_expr($expr)
 {
-    // variable de résultat
-    $result = "";
-    // tableau des nombres et opérateurs
-    $data = array();
+    // condition pour que l'array soit initialisé ou mis à jour avec la récursivité de la fonction
 
-    $subexpr = array();
-
-    $calc = false;
-
-    $sub_result = null;
-
-    $data_array = @create_array_with_float($expr);
-
-    $data = $data_array[0];
-
-    $decimals = $data_array[1];
-
-    // var_dump($data);
-    for ($i = 0; $i < count($data); $i++) {
-
-        if ($data[$i] === "(") {
-            $calc = true;
+    $numbers = array();
+    $array = str_split(str_replace(" ", "", $expr));
+    $res = "";
+    $operators = array();
+    $operators_values = array('-', '+', '/', '*', '%');
+    $number = "";
+    $counter = 0;
+    $count = false;
+    foreach ($array as $key => $value) {
+        if ($value === "(") {
+            $count = true;
+            array_push($operators, $value);
         }
 
-        if ($data[$i] === ")" && $calc === true) {
-            $calc = false;
-            $sub_result = @calc_subexpr($subexpr);
-            $result .= $sub_result;
+        if ($count === true) {
+            $counter++;
         }
 
-        if ($calc === true) {
-            array_push($subexpr, $data[$i]);
-        } elseif($calc === false && $data[$i] !== ")") {
-            $result .= $data[$i];
-        }
-    }
-
-    if (array_search(")",str_split($result)) !== false || array_search("(",str_split($result)) !== false) {
-        @eval_expr($result);
-    }
-
-    // echo "result : ".$result."\nsub_result : ".$sub_result."\n";
-    $sub_result = @calc_subexpr(create_array_with_float($result)[0],true);
-
-    if ($sub_result===true) {
-        @eval_expr($result);
-    }
-
-    return (double) $sub_result==""?$result."\n":$sub_result."\n";
-}
-
-
-function create_array_with_float($str){
-    //create array without spaces
-    $str = str_replace(" ","",$str);
-    $array1 = str_split($str);
-    $array_with_float = array();
-    $number = null;
-    $j=0;
-    for ($i=0; $i < count($array1); $i++) { 
-        if (($array1[$i]!=="%"&&$array1[$i]!=="+")&&($array1[$i]!=="-"&&$array1[$i]!=="/")&&($array1[$i]!=="*"&&$array1[$i]!=="("&&$array1[$i]!==")")) {
-            $number.=$array1[$i];
-            if ($array1[$i-1]===".") {
-                $j++;
-            }
-            if ($i==count($array1)-1) {
-                // echo $number;
-                // echo "number : ".$number."\narray i : ".$array1[$i]."\n";
-                $last_number = $number;
-                array_push($array_with_float,$last_number);
-            }
-        }else{
-            array_push($array_with_float,$number);
-            array_push($array_with_float,$array1[$i]);
-            $number = null;
-        }
-    }
-
-    // var_dump($array_with_float);
-
-    return [$array_with_float,$j];
-}
-
-function calc_subexpr($subexpr,$check=false)
-{
-    
-    $last_calc = null;
-    $sub_result = null;
-    if ($check===false) {
-        
-        for ($i = 0; $i < count($subexpr); $i++) {
-            // echo "sub_result : ".$sub_result." count : $i\n";
-            switch ($subexpr[$i]) {
+        if ($value === ")") {
+            $count = false;
+            $res = 0;
+            $prev = $numbers[count($numbers) - 2];
+            $last = $numbers[count($numbers) - 1];
+            array_push($numbers, array_pop($operators));
+            $opera = $numbers[count($numbers) - 1];
+            switch ($opera) {
                 case '+':
-                    if ($subexpr[$i+2]==="/"||$subexpr[$i+2]==="%"||$subexpr[$i+2]==="*") {
-                        $last_calc .= '+'.$subexpr[$i-1];
-                        // echo "subexxxxpr".$subexpr[$i-1]."\n last_calc : ".$last_calc."\n";
-                        $sub_result= null;
-                }else{
-                    $sub_result = $sub_result === null?$subexpr[$i - 1] + $subexpr[$i + 1]:$sub_result + $subexpr[$i+1];
+                    $res = intval($prev) + intval($last);
+                    $numbers[count($numbers) - 3] = "";
+                    $numbers[count($numbers) - 2] = "";
+                    $numbers[count($numbers) - 1] = "";
+                    $arrays = update_array($numbers, $operators);
+                    $numbers = $arrays[0];
+                    $operators = $arrays[1];
+                    array_push($numbers, $res);
+                    break;
+                case '-':
+                    $res = intval($prev) - intval($last);
+                    $numbers[count($numbers) - 3] = "";
+                    $numbers[count($numbers) - 2] = "";
+                    $numbers[count($numbers) - 1] = "";
+                    $arrays = update_array($numbers, $operators);
+                    $numbers = $arrays[0];
+                    $operators = $arrays[1];
+                    array_push($numbers, $res);
+                    break;
+                case '*':
+                    $res = intval($prev) * intval($last);
+                    $numbers[count($numbers) - 3] = "";
+                    $numbers[count($numbers) - 2] = "";
+                    $numbers[count($numbers) - 1] = "";
+                    $arrays = update_array($numbers, $operators);
+                    $numbers = $arrays[0];
+                    $operators = $arrays[1];
+                    array_push($numbers, $res);
+                    break;
+                case '/':
+                    $res = intval($prev) / intval($last);
+                    $numbers[count($numbers) - 3] = "";
+                    $numbers[count($numbers) - 2] = "";
+                    $numbers[count($numbers) - 1] = "";
+                    $arrays = update_array($numbers, $operators);
+                    $numbers = $arrays[0];
+                    $operators = $arrays[1];
+                    array_push($numbers, $res);
+                    break;
+                case '%':
+                    $res = intval($prev) % intval($last);
+                    $numbers[count($numbers) - 3] = "";
+                    $numbers[count($numbers) - 2] = "";
+                    $numbers[count($numbers) - 1] = "";
+
+                    $arrays = update_array($numbers, $operators);
+                    $numbers = $arrays[0];
+                    $operators = $arrays[1];
+                    array_push($numbers, $res);
+                    break;
+                case '(':
+                    array_pop($numbers);
+                    array_push($numbers, $res);
+                    break;
+                default:
+
+                    break;
+            }
+            $counter = 0;
+        }
+
+        if (is_numeric($value) && is_numeric($array[$key + 1])) {
+            $number .= $value;
+        }
+
+        if (in_array($value, $operators_values)) {
+            array_push($operators, $value);
+        }
+
+        if (is_numeric($value) && !is_numeric($array[$key + 1])) {
+            if ($number !== "") {
+                $number .= $value;
+                array_push($numbers, $number);
+            } else {
+                array_push($numbers, $value);
+            }
+            $number = "";
+        }
+    }
+
+    $i = 0;
+    while ($i < count($numbers) + 1) {
+        if (count($numbers) > 2 && $numbers[count($numbers) - 1] <= -1) {
+            $pop_negative = array_pop($numbers);
+            array_unshift($numbers, $pop_negative);
+            $operators = array_reverse($operators);
+        }
+        $prev = $numbers[count($numbers) - 2];
+        $last = $numbers[count($numbers) - 1];
+        array_push($numbers, $operators[count($operators) - 1]);
+        $opera = $numbers[count($numbers) - 1];
+
+        $res = 0;
+        switch ($opera) {
+            case '+':
+                $res = intval($prev) + intval($last);
+                if (count($numbers) > 2) {
+                    $numbers[count($numbers) - 3] = "";
                 }
-                $i++;
+                $numbers[count($numbers) - 2] = "";
+                $numbers[count($numbers) - 1] = "";
+                $arrays = update_array($numbers, $operators);
+                $numbers = $arrays[0];
+                $operators = $arrays[1];
+                array_push($numbers, $res);
                 break;
             case '-':
-                $sub_result = $sub_result === null?$subexpr[$i - 1] - $subexpr[$i + 1]:$sub_result - $subexpr[$i+1];
-                $i++;
-                break;
-            case '/':
-                $sub_result = $sub_result === null?$subexpr[$i - 1] / $subexpr[$i + 1]:$sub_result / $subexpr[$i+1];
-                // echo "last : ".$subexpr[$i - 1]."\n";
-                // echo "next : ".$subexpr[$i + 1]."\n";
-                $i++;
+                $res = intval($prev) - intval($last);
+                if (count($numbers) > 2) {
+                    $numbers[count($numbers) - 3] = "";
+                }
+                $numbers[count($numbers) - 2] = "";
+                $numbers[count($numbers) - 1] = "";
+                $arrays = update_array($numbers, $operators);
+                $numbers = $arrays[0];
+                $operators = $arrays[1];
+                array_push($numbers, $res);
                 break;
             case '*':
-                $sub_result = $sub_result === null?$subexpr[$i - 1] * $subexpr[$i + 1]:$sub_result * $subexpr[$i+1];
-                $i++;
+                $res = intval($prev) * intval($last);
+                if (count($numbers) > 2) {
+                    $numbers[count($numbers) - 3] = "";
+                }
+                $numbers[count($numbers) - 2] = "";
+                $numbers[count($numbers) - 1] = "";
+                $arrays = update_array($numbers, $operators);
+                $numbers = $arrays[0];
+                $operators = $arrays[1];
+                array_push($numbers, $res);
+                break;
+            case '/':
+                $res = intval($prev) / intval($last);
+                if (count($numbers) > 2) {
+                    $numbers[count($numbers) - 3] = "";
+                }
+                $numbers[count($numbers) - 2] = "";
+                $numbers[count($numbers) - 1] = "";
+                $arrays = update_array($numbers, $operators);
+                $numbers = $arrays[0];
+                $operators = $arrays[1];
+                array_push($numbers, $res);
                 break;
             case '%':
-                $sub_result = $sub_result === null?$subexpr[$i - 1] % $subexpr[$i + 1]:$sub_result % $subexpr[$i+1];
-                $i++;
+                $res = intval($prev) % intval($last);
+                if (count($numbers) > 2) {
+                    $numbers[count($numbers) - 3] = "";
+                }
+                $numbers[count($numbers) - 2] = "";
+                $numbers[count($numbers) - 1] = "";
+                $arrays = update_array($numbers, $operators);
+                $numbers = $arrays[0];
+                $operators = $arrays[1];
+                array_push($numbers, $res);
+                break;
+            case '(':
+                array_pop($numbers);
+                array_push($numbers, $res);
                 break;
             default:
-            break;
+
+                break;
         }
-        
+        $i++;
     }
-    if ($last_calc!== null) {
-        $sub_result.= $last_calc;
-        // echo "sub_result : ".$sub_result."\n";
-        $sub_result=@calc_subexpr(create_array_with_float($sub_result)[0]);
-        // echo "sub_result : ".$sub_result."\n";
-    }
-    return $sub_result;
-}else{
-    foreach ($subexpr as $key => $value) {
-        if (!is_numeric($value)&&$value!=".") {
-            // echo "value : ".$value;
-            return @calc_subexpr($subexpr);
-        }
-    }
+    return "$res\n";
 }
 
+function update_array($numbers, $operators)
+{
+    $array1 = [];
+    $array2 = [];
 
+    foreach ($numbers as $key => $value) {
+        if ($value != NULL || $value != "") {
+            array_push($array1, $value);
+        }
+    }
+
+    foreach ($operators as $key => $value) {
+        if ($key !== count($operators) - 1 && $value !== "(") {
+            array_push($array2, $value);
+        }
+    }
+
+    return [$array1, $array2];
 }
--- tester.php
+++ tester.php
@@ -1,4 +1,4 @@
 <?php
 require(__DIR__."/index.php");
 $expr = $argv[1];
-echo eval_expr($expr);
+echo @eval_expr($expr);
